1a8476343fc22213ebf67db081bacc20230d10ba
Grant Cox <42614867+gcox32@users.noreply.github.com>
2024-12-12 22:28:54 -0500

    Update next.config.ts to add env vars

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
           },
       ],
   },
+  env: {
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID || '',
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY || '',
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || '',
+  }
 }
 
 
