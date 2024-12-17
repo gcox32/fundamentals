8e34fe1e0d437cbc0b677e58fa743620a50768c7
Grant Cox <42614867+gcox32@users.noreply.github.com>
2024-12-17 16:25:37 -0500

    Add env vars to next config

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,10 @@ const nextConfig: NextConfig = {
   env: {
     ACCESS_KEY_ID: process.env.ACCESS_KEY_ID || '',
     SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY || '',
-    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || ''
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || '',
+    FMP_API_KEY: process.env.FMP_API_KEY || '',
+    RAPID_API_KEY: process.env.RAPID_API_KEY || '',
+    REGION: process.env.REGION || 'us-east-1',
   }
 }
 
